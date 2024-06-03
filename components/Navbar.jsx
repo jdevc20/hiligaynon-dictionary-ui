7452ea690a19ced562c8a5bdd44130edcf867d8d
jdevc920 <jdcamarin20@gmail.com>
2024-06-04 00:23:14 +0800

    [*]

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -23,7 +23,7 @@ function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-dark">
       <div className="container">
         <Link href="/" legacyBehavior>
-          <a className="navbar-brand">Hiligaynon Gid</a>
+          <a className="navbar-brand">Hiligaynon</a>
         </Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
--- pages/dictionary/[word].js
+++ pages/dictionary/[word].js
@@ -1,83 +1,120 @@
-import React from 'react';
-import { useRouter } from 'next/router';
+import React from "react";
+import { useRouter } from "next/router";
 
 const fetchWordData = async (word) => {
-    try {
-        const response = await fetch(`https://ilonggogid-api.onrender.com/dictionary/words/by-word/${word}`);
-        if (!response.ok) {
-            throw new Error('Failed to fetch word data');
-        }
-        return await response.json();
-    } catch (error) {
-        console.error('Error fetching word data:', error);
-        return null;
+  try {
+    const response = await fetch(
+      `https://ilonggogid-api.onrender.com/dictionary/words/by-word/${word}`
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch word data");
     }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching word data:", error);
+    return null;
+  }
 };
 
 const WordDetail = ({ wordData }) => {
-    const router = useRouter();
-    const handleSpeak = () => {
-        // Leave Blank for now
-    };
+  const router = useRouter();
+  const handleSpeak = () => {
+    // Leave Blank for now
+  };
 
-    if (!wordData || wordData.length === 0) {
-        return <div className='container mt-5 text-white d-flex justify-content-center align-items-center' style={{ height: '100vh' }}>Word is not available yet.</div>;
-    }
-
+  if (!wordData || wordData.length === 0) {
     return (
-        <div className='container mt-5'>
-            <button className="btn btn-back" onClick={() => router.back()}><i className="fa fa-chevron-circle-left" aria-hidden="true"></i></button>
-            {wordData.map((wordItem, index) => (
-                <div key={index} className="wordDetailContainer roboto-mono-message mt-2">                   
-                    <div className="container">
-                        <h2>{wordItem.word}</h2>
-                        <p className={`mb-0 ${wordItem.wordType ? wordItem.wordType.toLowerCase() : ''}`}>
-                            {wordItem.wordType}
-                        </p>
-                        <div className="pronunciation">
-                            <span className="pronunciation-text">Pronunciation: {wordItem.pronunciation}</span>
-                            <button className="btn btn-speak" onClick={handleSpeak}>
-                                <i className="fa fa-volume-up" aria-hidden="true"></i> Speak
-                            </button>
-                        </div>
-                        <label>Definitions:</label>
-                        {wordItem.definitions.map((definition, defIndex) => (
-                            <div key={defIndex} className="definition-container">
-                                <strong>{definition.language}: </strong>
-                                {definition.definition}
-                            </div>
-                        ))}
-
-                        <label>Examples:</label>
-                        <ul>
-                            {wordItem.examples.map((example, exampleIndex) => (
-                                <li key={exampleIndex}>
-                                    <em>{example.example}</em> - {example.translation}
-                                </li>
-                            ))}
-                        </ul>
-                        {wordItem.furtherDetails && (
-                            <p className="mb-0"><strong>Additional details :</strong> {wordItem.furtherDetails}</p>
-                        )}
-                        {wordItem.etymology && (
-                            <p className="mb-0"><strong>Etymology:</strong> {wordItem.etymology}</p>
-                        )}
-                    </div>
-                </div>
-            ))}
-        </div>
+      <div
+        className="container mt-5 text-white d-flex justify-content-center align-items-center"
+        style={{ height: "100vh" }}
+      >
+        Word is not available yet.
+      </div>
     );
-}
+  }
+
+  return (
+    <div className="container mt-3 mb-5 pb-2">
+      <button className="btn btn-back" onClick={() => router.back()}>
+        <i className="fa fa-chevron-circle-left" aria-hidden="true"></i>
+      </button>
+      {wordData.map((wordItem, index) => (
+        <div
+          key={index}
+          className="wordDetailContainer roboto-mono-message mt-2"
+        >
+          <div className="container">
+            <h2>{wordItem.word}</h2>
+            <p
+              className={`mb-0 ${
+                wordItem.wordType ? wordItem.wordType.toLowerCase() : ""
+              }`}
+            >
+              {wordItem.wordType}
+            </p>
+            {/* Display spelling variations */}
+            {wordItem.spellingVariations && (
+              <p>{wordItem.spellingVariations.join(", ")}</p>
+            )}
+            <div className="pronunciation d-flex">
+              <button className="btn btn-speak" onClick={handleSpeak}>
+                <i className="fa fa-volume-up mx-1" aria-hidden="true"></i>
+                 Pronunciation: {wordItem.pronunciation}
+              </button>
+            </div>
+
+            <label>Definitions:</label>
+            {wordItem.definitions.length > 0 ? (
+              wordItem.definitions.map((definition, defIndex) => (
+                <div key={defIndex} className="definition-container">
+                  <strong>{definition.language}: </strong>
+                  {definition.definition}
+                </div>
+              ))
+            ) : (
+              <p>No definition yet.</p>
+            )}
+            
+            <br />
+            <label>Examples:</label>
+            <ul>
+              {wordItem.examples.length > 0 ? (
+                wordItem.examples.map((example, exampleIndex) => (
+                  <li key={exampleIndex}>
+                    <em>{example.example}</em> - {example.translation}
+                  </li>
+                ))
+              ) : (
+                <p>No examples yet.</p>
+              )}
+            </ul>
+            <hr />
+            {wordItem.furtherDetails && (
+              <p className="mb-0">
+                <strong>Additional details :</strong> {wordItem.furtherDetails}
+              </p>
+            )}
+            {wordItem.etymology && (
+              <p className="mb-0">
+                <strong>Etymology:</strong> {wordItem.etymology}
+              </p>
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export async function getServerSideProps(context) {
-    const { word } = context.params;
-    const wordData = await fetchWordData(word);
+  const { word } = context.params;
+  const wordData = await fetchWordData(word);
 
-    return {
-        props: {
-            wordData,
-        },
-    };
+  return {
+    props: {
+      wordData,
+    },
+  };
 }
 
 export default WordDetail;
