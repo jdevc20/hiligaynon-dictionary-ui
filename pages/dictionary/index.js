8af4235bc71ab17046aec89fc05271fafcc09124
jdevc29 <jdcamarin20@gmail.com>
2024-06-25 11:47:23 +0800

    tyjhyerthj

--- pages/dictionary/index.js
+++ pages/dictionary/index.js
@@ -3,32 +3,33 @@ import { useRouter } from "next/router";
 import DictionaryWordItem from "@/components/DictionaryWordItem";
 import api from "../api/api";
 import AlphabetPagination from "@/components/AlphabetPagination";
+import debounce from "lodash.debounce";
 
 function Dictionary() {
-  const [selectedWord, setSelectedWord] = useState(null);
-  const [words, setWords] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
-
   const router = useRouter();
   const { query } = router;
 
-  useEffect(() => {
-    // Default to letter 'a' if query.letter is not present
-    const initialLetter = query.letter || "a";
-    fetchWords(initialLetter);
+  const [words, setWords] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoadingWord, setIsLoadingWord] = useState(false);
 
-    // Redirect to /dictionary?letter=a if no query parameter is present
+  useEffect(() => {
+    const initialLetter = query.letter || "a";
+    fetchWords(initialLetter, searchTerm);
     if (!query.letter) {
       router.replace("/dictionary?letter=a");
     }
-  }, [query.letter]);
+  }, [query.letter, searchTerm]);
 
-  const fetchWords = async (letter) => {
+  const fetchWords = async (letter, searchTerm = "") => {
     setIsLoading(true);
     try {
-      const wordsData = await api.get(`/dictionary/words?letter=${letter}`);
-      setWords(wordsData);
+      let endpoint = searchTerm
+        ? `/dictionary/search?query=${encodeURIComponent(searchTerm)}`
+        : `/dictionary/words?letter=${letter}`;
+      const response = await api.get(endpoint);
+      setWords(response.data); 
     } catch (error) {
       console.error("Error fetching words:", error);
     } finally {
@@ -36,17 +37,24 @@ function Dictionary() {
     }
   };
 
-  const handleWordSelect = (word) => {
-    router.push(`/dictionary/${word.word}`);
-  };
-
-  const handleBackButtonClick = () => {
-    setSelectedWord(null);
-  };
+  const debouncedSearch = debounce((searchTerm) => {
+    // Only perform search if searchTerm is longer than 2 characters or empty
+    if (searchTerm.length > 2 || searchTerm.length === 0) {
+      fetchWords(query.letter || 'a', searchTerm);
+    } else {
+      setWords([]); // Clear previous search results or set to initial state
+    }
+  }, 300);
 
   const handleSearchChange = (event) => {
     const searchTerm = removeAccents(event.target.value);
     setSearchTerm(searchTerm);
+    debouncedSearch(searchTerm);
+  };
+
+  const handleWordSelect = (word) => {
+    setIsLoadingWord(true);
+    router.push(`/dictionary/${word.word}`);
   };
 
   const removeAccents = (str) => {
@@ -80,32 +88,33 @@ function Dictionary() {
           </div>
           <div className="col-md-9">
             <div className="resultContainer">
-        {isLoading ? (
-          <div className="loadingContainer">Loading Dictionary...</div>
-        ) : selectedWord ? (
-          <div className="container detail-container">
-            {/* Render detailed view of selected word */}
-          </div>
-        ) : (
-          <>
-            <div className="wordGrid">
-              {filteredWords.map((word, index) => (
-                <DictionaryWordItem
-                  key={index}
-                  word={word}
-                  definition={word.definition}
-                  onSelect={() => handleWordSelect(word)}
-                />
-              ))}
+              {isLoading ? (
+                <div className="loadingContainer">Loading Dictionary...</div>
+              ) : (
+                <>
+                  {isLoadingWord && (
+                    <div className="loadingContainer">Getting Word Definition...</div>
+                  )}
+                  {!isLoadingWord && filteredWords.length > 0 ? (
+                    <div className="wordGrid">
+                      {filteredWords.map((word, index) => (
+                        <DictionaryWordItem
+                          key={index}
+                          word={word}
+                          definition={word.definition} // Adjust as per your data structure
+                          onSelect={() => handleWordSelect(word)}
+                        />
+                      ))}
+                    </div>
+                  ) : (
+                    <div>No words found.</div>
+                  )}
+                </>
+              )}
             </div>
-          </>
-        )}
-      </div>
           </div>
         </div>
       </div>
-
-
     </div>
   );
 }
